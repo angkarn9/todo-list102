556b8ab4847d87f2e508de67ff8a00dc00b10257
Angkarn Janjuang <angkarn9@gmail.com>
2020-03-01 13:02:38 +0700

    [create test case add]

--- src/app/service/todo-list.service.spec.ts
+++ src/app/service/todo-list.service.spec.ts
@@ -3,6 +3,7 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { TodoListService } from './todo-list.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 fdescribe('TodoListService', () => {
   let service: TodoListService;
@@ -39,5 +40,64 @@ fdescribe('TodoListService', () => {
       caller.flush(expected);
       httpTestingController.verify();
     });
+
+    it('should return error message when http response fail', () => {
+      service.getTodoList().subscribe(() => {
+      }, (error: HttpErrorResponse) => {
+        expect(error.status === 400);
+        expect(error.statusText === 'Bad request');
+      });
+
+      const caller = httpTestingController.expectOne('http://www.mocky.io/v2/5e5b456d3000000e00f9f1e7');
+      expect(caller.request.method).toEqual('GET');
+      caller.flush('', {
+        status: 400, statusText: 'Bad request'
+      });
+    });
+  });
+
+  describe('add', () => {
+    it('should call http POST with todoList and http status response = 201', () => {
+      const param = {
+        id: 2,
+        topic: 'Topic1',
+        description: 'Desc1',
+      };
+      service.add(param).subscribe(() => {
+
+      });
+
+      const expected = {
+        id: 2,
+        topic: 'Topic1',
+        description: 'Desc1',
+      };
+      const caller = httpTestingController.expectOne('http://www.mocky.io/v2/5e5b4cf23000004c00f9f1f0');
+      expect(caller.request.method).toEqual('POST');
+      expect(caller.request.body).toEqual(expected);
+      caller.flush({});
+      httpTestingController.verify();
+    });
+
+    it('should return error message when http response fail', () => {
+      const param = {
+        id: 3,
+        topic: '',
+        description: '',
+      };
+      service.add(param).subscribe(() => {
+
+      }, (error: HttpErrorResponse) => {
+        expect(error.status === 400);
+        expect(error.statusText === 'Bad request');
+      });
+
+      const caller = httpTestingController.expectOne('http://www.mocky.io/v2/5e5b4cf23000004c00f9f1f0');
+      caller.flush('', {
+        status: 400, statusText: 'Bad request'
+      });
+    });
+
+
   });
 });
--- src/app/service/todo-list.service.ts
+++ src/app/service/todo-list.service.ts
@@ -15,4 +15,8 @@ export class TodoListService {
   getTodoList(): Observable<TodoList[]> {
     return this.httpClient.get<TodoList[]>('http://www.mocky.io/v2/5e5b456d3000000e00f9f1e7');
   }
+
+  add(param: TodoList): Observable<any> {
+    return this.httpClient.post('http://www.mocky.io/v2/5e5b4cf23000004c00f9f1f0', param);
+  }
 }
