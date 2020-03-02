96529ad6964fcf06782e145201dd7f7e215d9721
Angkarn Janjuang <angkarn9@gmail.com>
2020-03-02 22:08:30 +0700

    create test form

--- src/app/todo-manage/todo-manage.component.spec.ts
+++ src/app/todo-manage/todo-manage.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoManageComponent } from './todo-manage.component';
+import { FormGroup, FormControl } from '@angular/forms';
 
 describe('TodoManageComponent', () => {
   let component: TodoManageComponent;
@@ -22,4 +23,17 @@ describe('TodoManageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create todoListForm', () => {
+    expect(component.todoListForm instanceof FormGroup).toBe(true);
+  });
+
+  it('should create fields topic and description in todoListForm', () => {
+    const expected = new FormGroup({
+      topic: new FormControl(''),
+      description: new FormControl('')
+    });
+    expect(component.todoListForm.getRawValue()).toEqual(expected.getRawValue());
+  });
+
 });
--- src/app/todo-manage/todo-manage.component.ts
+++ src/app/todo-manage/todo-manage.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-manage.component.scss']
 })
 export class TodoManageComponent implements OnInit {
+  todoListForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.todoListForm = new FormGroup({
+      topic: new FormControl(''),
+      description: new FormControl('')
+    });
   }
 
 }
